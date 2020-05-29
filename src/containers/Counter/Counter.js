fa9ca4296b0f0447213ea702f0295eb27e971ea3
Abhishek <asharma102@sapient.com>
2020-05-29 12:32:29 +0100

    adding redux in react

---
+++ src/containers/Counter/Counter.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import CounterControl from '../../components/CounterControl/CounterControl';
+import CounterOutput from '../../components/CounterOutput/CounterOutput';
+
+class Counter extends Component {
+
+
+
+    render() {
+        return (
+            <div>
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 5" clicked={this.props.onAdd} />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtract} />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
+        onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
+        onAdd: () => dispatch({ type: 'ADD', val: 5 }),
+        onSubtract: () => dispatch({ type: 'SUBTRACT', val: 5 })
+
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
